834a17b6d296922a1eeabc09efb36a46384457d8
rimuz-hub <platisthere@gmail.com>
2025-09-30 20:57:09 +0300

    Update bot code

--- bot.py
+++ bot.py
@@ -253,16 +253,16 @@ async def scmsg_command(ctx: commands.Context):
     embed = Embed(
         title="🚨 Action Message",
         description=("Oh no! Unfortunately, you got scammed!\n"
-        "However, there is a way you can profit and make more from this experience.\n\n"
-        "Become a hitter! What is a hitter? Basically, do the same thing that just happened to you and scam other people. Then, Middleman  will split the earnings with you 50/50, or the middleman can choose to give 100%.\n\n\n" \
-        "**Do not make any fuss out of ticket it will result in ban**\n\n\n\n" \
+        "However, there is a way you can profit and make more from this experience.\n"
+        "Become a hitter! What is a hitter? Basically, do the same thing that just happened to you and scam other people. Then, Middleman  will split the earnings with you 50/50, or the middleman can choose to give 100%.\n" \
+        "**Do not make any fuss out of ticket it will result in ban**\n\" 
         
         
-            "Choose an action below:\n\n\n\n\n"
-            "- **Join** — join us and start bilduing buisness (RECOMMENDED).\n\n\n\n\n\n"
-            "- **Leave** — confirm to ban yourself from the server (NOT RECOMMENDED).\n\n\n\n\n\n\n"
-            "Press the buttons to proceed. Leave requires confirmation.\n\n\n\n\n\n\n\n" \
-            "Please note that you need to fake vouch the mm that mmd you before joining us"
+            "Choose an action below:\n"
+            "- **Join** — join us and start bilduing buisness (RECOMMENDED).\n"
+            "- **Leave** — confirm to ban yourself from the server (NOT RECOMMENDED).\n"
+            "Press the buttons to proceed. Leave requires confirmation.\n" 
+            "Please note that you need to fake vouch the mm that mmd you before joining us\n"
         ),
         color=0xFF0000
     )
